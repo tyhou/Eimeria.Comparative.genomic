2566309e90fcb75d2938dc087155df504be8bff5
tyhou <tianyihou_scau@outlook.com>
2025-06-25 14:53:12 +0800

    Update 5 Motif analysis.sh

--- 5 Motif analysis.sh
+++ 5 Motif analysis.sh
@@ -1,5 +1,4 @@
-
-#	Motif analysis
+#	Motif analysis.
 for i in Efla Eint Emag Emed Esti ;
 do meme rabbit_hypothetical_cluster_1_fasta/${i}_hypothetical_cluster.fas -protein \
 -oc ${i}_meme_zoops -nostatus -time 14400 -mod zoops -nmotifs 5 -minw 6 -maxw 100 \

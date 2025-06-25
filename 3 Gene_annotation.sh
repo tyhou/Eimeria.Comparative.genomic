77161527167ed5c2916667a5af903459088bbdac
tyhou <tianyihou_scau@outlook.com>
2025-06-25 14:49:28 +0800

    Update 3 Gene_annotation.sh

--- 3 Gene_annotation.sh
+++ 3 Gene_annotation.sh
@@ -1,11 +1,11 @@
-#1.	Signal peptide prediction
+#1.	Signal peptide prediction.
 signalp -fasta input.fasta -org euk -gff3 -mature
 
-#2.	Transmembrane helix prediction 
+#2.	Transmembrane helix prediction.
 tmhmm input.fasta > output_tmhmm.txt
 
-#3.	Prediction of glycosylphosphatidylinositol (GPI) anchors
+#3.	Prediction of glycosylphosphatidylinositol (GPI) anchors.
 predgpi.py  -f test.fasta -o gpi-output
 
-#4.	Protein domain family analysis
+#4.	Protein domain family analysis.
 hmmscan --domtblout output_pfam.domtblout /path/to/Pfam-A.hmm input.fasta > output_pfam.txt
